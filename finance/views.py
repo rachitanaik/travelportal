4aae56fae3c97dcbf1f12c2b00dc1b46d97b7ec2
Rachita <rachitanaik590@gmail.com>
2018-03-10 14:05:38 +0530

    added changes

--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse('<h1>Finance</h1>')
 
 # Create your views here.
--- travelportal/urls.py
+++ travelportal/urls.py
@@ -1,12 +1,27 @@
-from django.conf.urls import patterns, include, url
+"""travelportal URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-admin.autodiscover()
+from django.urls import path,include
+from django.contrib.auth import views as auth_views
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'travelportal.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = [
+    path('', auth_views.login,name='login'),
+    path('employee/', include('employee.urls')),
+    path('finance/', include('finance.urls')),
+    path('vendor/', include('vendor.urls')),
+    path('manager/', include('manager.urls')),
+    	]
 
-    url(r'^admin/', include(admin.site.urls)),
-)
